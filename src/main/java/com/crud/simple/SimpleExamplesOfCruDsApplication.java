21355021f699c44074e7cf86e190fda5598f8b12
dayvson_menezes_local <dayvison.assis@gmail.com>
2023-06-22 21:25:41 -0300

    start

---
+++ src/main/java/com/crud/simple/SimpleExamplesOfCruDsApplication.java
@@ -0,0 +1,13 @@
+package com.crud.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleExamplesOfCruDsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleExamplesOfCruDsApplication.class, args);
+	}
+
+}
